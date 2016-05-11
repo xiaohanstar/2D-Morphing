746811772b071dee60de4cb96e8d52667ab177f1
Dawncoldstar <xiaohanstar@gmail.com>
2016-05-11 13:12:47 +0800

    Update 2D Blending Demo for FYP.vb

--- 2D Blending Demo for FYP.vb
+++ 2D Blending Demo for FYP.vb
@@ -1,4 +1,4 @@
-﻿Imports System.IO
+Imports System.IO
 Imports System.Threading
 Imports Microsoft.VisualBasic.PowerPacks
 Imports Microsoft.VisualBasic.Strings
@@ -48,8 +48,8 @@ Public Class Form1
     'for edge tweakling
     Dim srcAngleArray(0) As Double
     Dim destAngleArray(0) As Double
-    Dim fromLength(0) As Double
-    Dim toLength(0) As Double
+    Dim LA(0) As Double
+    Dim LB(0) As Double
     Dim Lab(0) As Double
 
     ' Create array of points that define lines to draw.
@@ -93,7 +93,7 @@ Public Class Form1
         Dim n As Integer
         m = n = 0
         For i = 0 To NumVertexFrom - 2
-            If getFromAngle(i) = getFromAngle(i + 1) Then
+            If getfromAnlge(i) = getfromAnlge(i + 1) Then
                 countFrom += 1
             Else
                 ReDim CopyFromX(m)
@@ -103,16 +103,18 @@ Public Class Form1
         Next
 
         For i = 0 To NumVertexTo - 2
-            If getToAngle(i) = getToAngle(i + 1) Then
+            If gettoAngle(i) = gettoAngle(i + 1) Then
                 countTo += 1
             End If
         Next
     End Sub
     Private Sub MorphProgress(ByRef steps As Integer)
-        If Vertex_RBtn.Checked = True Then
-            VertexMorphingProgress(steps)
-        Else
-            EdgeMorphingProgress(steps)
+        If NtoOne_RBtn.Checked = True Or NtoOne_RBtn.Checked = False Then
+            If Vertex_RBtn.Checked = True Then
+                VertexMorphingProgress(steps)
+            Else
+                EdgeMorphingProgress(steps)
+            End If
         End If
     End Sub
 
@@ -226,12 +228,13 @@ Public Class Form1
 
     ' Next Button Function when Mouse Down (Click) event
     Private Sub Next_Click(sender As Object, e As EventArgs) Handles Next_Btn.Click
-        MorphStep += 1 '(100 / TotalStep)
-        If MorphStep >= TotalStep Then
+        If MorphStep >= TotalStep - 1 Then
             MorphStep = TotalStep
             Timeline_Bar.Value = TotalStep
+        Else
+            MorphStep += 1
+            Timeline_Bar.Value = MorphStep
         End If
-        Timeline_Bar.Value = MorphStep
         MorphProgress(MorphStep)
 
         Me.Refresh()
@@ -383,7 +386,7 @@ Public Class Form1
                     ' copy value of previous 
                     For j = numofVertex - 2 To i + 1
                         CopyToX(j + 1) = CopyToX(j)
-                        CopyToY(j + 1) = CopyToX(j)
+                        CopyToY(j + 1) = CopyToY(j)
                     Next
                     CopyToX(i + 1) = (CopyToX(i) + CopyToX(i + 2)) / 2
                     CopyToY(i + 1) = (CopyToY(i) + CopyToY(i + 2)) / 2
@@ -397,7 +400,7 @@ Public Class Form1
                     ' This may have issues on the last Index
                     For j = numofVertex - 2 To i + 1
                         CopyFromX(j + 1) = CopyFromX(j)
-                        CopyFromY(j + 1) = CopyFromX(j)
+                        CopyFromY(j + 1) = CopyFromY(j)
                     Next
                     CopyFromX(i + 1) = (CopyFromX(i) + CopyFromX(i + 2)) / 2
                     CopyFromY(i + 1) = (CopyFromY(i) + CopyFromY(i + 2)) / 2
@@ -460,7 +463,7 @@ Public Class Form1
                         ' copy value of previous 
                         For j = numofVertex - 2 To i + 1
                             MorphX(j + 1) = MorphX(j)
-                            MorphY(j + 1) = MorphX(j)
+                            MorphY(j + 1) = MorphY(j)
                         Next
                         MorphX(i + 1) = MorphX(i)
                         MorphY(i + 1) = MorphY(i)
@@ -500,15 +503,15 @@ Public Class Form1
             MorphY(i) = CopyFromY(i)
         Next
 
-        'Console.WriteLine(Counter)
     End Sub
     Private Sub VertexMorphingProgress(ByRef steps As Integer)
         'takes morph step as parameter
         MorphPoints.Clear()
+        Dim t As Double = steps / TotalStep
 
         For i = 0 To numofVertex - 1
-            MorphX(i) = CopyFromX(i) + (CopyToX(i) - CopyFromX(i)) * steps / TotalStep
-            MorphY(i) = CopyFromY(i) + (CopyToY(i) - CopyFromY(i)) * steps / TotalStep
+            MorphX(i) = CopyFromX(i) * (1 - t) + CopyToX(i) * t
+            MorphY(i) = CopyFromY(i) * (1 - t) + CopyToY(i) * t
         Next
 
         ' Write to Morph Corrdinate
@@ -516,7 +519,7 @@ Public Class Form1
             MorphPoints.Add(New Point(MorphX(i), MorphY(i)))
         Next
     End Sub
-    Private Function getleastanglediff(ByRef angle1 As Double, ByRef angle2 As Double) As Double
+    Private Function getleastAngleDiff(ByRef angle1 As Double, ByRef angle2 As Double) As Double
         Dim output_angle1 As Double
         Dim output_angle2 As Double
         regulateAngle(angle1)
@@ -524,7 +527,8 @@ Public Class Form1
 
         output_angle1 = angle2 - angle1
 
-        If (output_angle1 >= 0) Then
+        'output2 is output 1 within 
+        If (output_angle1 >= 0) Then 'angle2 bigger-> output2 = 1 - 2pi  
             output_angle2 = output_angle1 - Math.PI * 2
         Else
             output_angle2 = output_angle1 + Math.PI * 2
@@ -542,7 +546,7 @@ Public Class Form1
     Private Sub regulateAngle2(ByRef angle As Double)
         If angle > Math.PI Then
             angle = angle - 2 * Math.PI
-        ElseIf angle < (-1) * Math.PI Then
+        ElseIf angle <= (-1) * Math.PI Then
             angle = angle + 2 * PI
         End If
     End Sub
@@ -558,33 +562,25 @@ Public Class Form1
     End Sub
 
     'to return the angle between positive x axis and the line made of current vertex with previous vertex
-    Public Function getFromAngle(ByVal i As Integer) As Double
+    Public Function getfromAnlge(ByVal i As Integer) As Double
         Dim x1 As Double
         Dim y1 As Double
         Dim angle As Double
-        If i = 0 Then
-            x1 = CopyFromX(i) - CopyFromX(numofVertex - 2)
-            y1 = CopyFromY(i) - CopyFromY(numofVertex - 2)
-        Else
-            x1 = CopyFromX(i) - CopyFromX(i - 1)
-            y1 = CopyFromY(i) - CopyFromY(i - 1)
-        End If
+        x1 = CopyFromX(i + 1) - CopyFromX(i)
+        y1 = CopyFromY(i + 1) - CopyFromY(i)
 
         angle = Math.Atan2(y1, x1)
         'regulateAngle(angle)
         Return angle
     End Function
-    Public Function getToAngle(ByVal i As Integer) As Double
+    Public Function gettoAngle(ByVal i As Integer) As Double
         Dim x1 As Double
         Dim y1 As Double
         Dim angle As Double
-        If i = 0 Then
-            x1 = CopyToX(i) - CopyToX(numofVertex - 1)
-            y1 = CopyToY(i) - CopyToY(numofVertex - 1)
-        Else
-            x1 = CopyToX(i) - CopyToX(i - 1)
-            y1 = CopyToY(i) - CopyToY(i - 1)
-        End If
+
+        x1 = CopyToX(i + 1) - CopyToX(i)
+        y1 = CopyToY(i + 1) - CopyToY(i)
+
         angle = Math.Atan2(y1, x1)
         'regulateAngle(angle)
         Return angle
@@ -593,14 +589,19 @@ Public Class Form1
     Private Sub EdgeMorphingProgress(ByRef steps As Integer)
         MorphPoints.Clear()
         Dim t As Double = steps / TotalStep
-        Console.WriteLine(t)
-        Dim MorphAngle(numofVertex - 1) As Double
-        Dim fromAngle(numofVertex - 1) As Double
-        Dim toAngle(numofVertex - 1) As Double
+        Dim m As Integer = numofVertex - 2
+
+        Dim alpha(numofVertex - 1) As Double
+        Dim alphaA(numofVertex - 1) As Double
+        Dim alphaB(numofVertex - 1) As Double
+
+        Dim thetaA(numofVertex - 1) As Double
+        Dim thetaB(numofVertex - 1) As Double
+        Dim theta(numofVertex - 1) As Double
 
         Dim s(numofVertex - 1) As Double
-        ReDim fromLength(numofVertex - 1)
-        ReDim toLength(numofVertex - 1)
+        ReDim LA(numofVertex - 1)
+        ReDim LB(numofVertex - 1)
         Dim MorphL(numofVertex - 1) As Double
         Dim newLength1(numofVertex - 1) As Double ' this is (1-t)La + tLB before edge tweakling
 
@@ -609,51 +610,40 @@ Public Class Form1
         Dim ToX1 As Double
         Dim ToY1 As Double
 
-        'initialize all morphangle and morphx and morphy
-        For i = 0 To numofVertex - 1
-            fromAngle(i) = getFromAngle(i)
-            toAngle(i) = getToAngle(i)
-            'MorphAngle(i) = fromAngle(i)
-            If i > 0 Then
-                fromX1 = CopyFromX(i) - CopyFromX(i - 1)
-                fromY1 = CopyFromY(i) - CopyFromY(i - 1)
-                ToX1 = CopyToX(i) - CopyToX(i - 1)
-                ToY1 = CopyToY(i) - CopyToY(i - 1)
-                fromLength(i) = Math.Sqrt(fromX1 * fromX1 + fromY1 * fromY1)
-                toLength(i) = Math.Sqrt(ToX1 * ToX1 + ToY1 * ToY1)
-            End If
+        'initialize all alpha and morphx and morphy
+        For i = 0 To m
+            fromX1 = CopyFromX(i + 1) - CopyFromX(i)
+            fromY1 = CopyFromY(i + 1) - CopyFromY(i)
+            ToX1 = CopyToX(i + 1) - CopyToX(i)
+            ToY1 = CopyToY(i + 1) - CopyToY(i)
+            LA(i) = Math.Sqrt(fromX1 * fromX1 + fromY1 * fromY1)
+            LB(i) = Math.Sqrt(ToX1 * ToX1 + ToY1 * ToY1)
+            'below computes new Length before tweakling
+            newLength1(i) = LA(i) * (1 - t) + LB(i) * t
+
+            alphaA(i) = Math.Atan2(fromY1, fromX1)
+            alphaB(i) = Math.Atan2(ToY1, ToX1)
         Next
 
-        fromLength(0) = fromLength(numofVertex - 1)
-        toLength(0) = toLength(numofVertex - 1)
-        s(0) = 0
-        newLength1(0) = 0
-        MorphL(0) = 0
-        ' end of initialization
-
-        Dim fromAngleChange As Double
-        Dim toAngleChange As Double
-        Dim newChange As Double
-
+        ' end of initialization B-A
+        alpha(0) = alphaA(0) + getleastanglediff(alphaA(0), alphaB(0)) * steps / TotalStep
         'below computes new angle changes
-        MorphAngle(0) = 0
-        For i = 1 To numofVertex - 1
-            fromAngleChange = fromAngle(i) - fromAngle(i - 1)
-            toAngleChange = toAngle(i) - toAngle(i - 1)
-            regulateAngle2(fromAngleChange)
-            regulateAngle2(toAngleChange)
+        'alpha(0) = alphaA(0) * (1 - t) + alphaB(0) * t
+        For i = 1 To m
+            thetaA(i) = alphaA(i) - alphaA(i - 1)
+            thetaB(i) = alphaB(i) - alphaB(i - 1)
 
-            'newChange value is in between -pi to pi
-            newChange = fromAngleChange * (1 - t) + toAngleChange * steps / TotalStep
-            MorphAngle(i) = MorphAngle(i - 1) + newChange '- Math.PI
-            regulateAngle2(MorphAngle(i))
-        Next
+            While thetaA(i) > Math.PI Or thetaA(i) <= -Math.PI
+                regulateAngle2(thetaA(i))
+            End While
+            While thetaB(i) > Math.PI Or thetaB(i) < -Math.PI
+                regulateAngle2(thetaB(i))
+            End While
 
-        'below computes new Length before tweakling
-        newLength1(0) = 0
-        For i = 1 To numofVertex - 1
-            'below are vector lines  to compute new length of i to i-1
-            newLength1(i) = fromLength(i) * (1 - t) + toLength(i) * t
+            'theta value is in between -pi to pi
+            theta(i) = thetaA(i) * (1 - t) + thetaB(i) * t
+            alpha(i) = alpha(i - 1) + theta(i)
+            regulateAngle(alpha(i)) 'alpha is for get its sin and cos, no need to regulate
         Next
 
         'compute LAB as global variable
@@ -662,47 +652,44 @@ Public Class Form1
         Dim Ltol As Double = ComputeLtol()
 
         For i = 0 To numofVertex - 1
-            edge_difference = Math.Abs(fromLength(i) - toLength(i))
+            edge_difference = Math.Abs(LA(i) - LB(i))
             If edge_difference >= Ltol Then
                 Lab(i) = edge_difference
             Else
                 Lab(i) = Ltol
             End If
-            Console.WriteLine(Lab(i))
         Next
 
         'calculate for morphx and morph y for last vertex first  which is the same postition as i =0
         For i = 0 To numofVertex - 1
+
+            'use edge tweakling to close the shape
+            Dim e As Double = ComputeE(alpha)
+            Dim f As Double = ComputeF(alpha)
+            Dim g As Double = ComputeG(alpha)
+            Dim u As Double = ComputeU(newLength1, alpha)
+            Dim v As Double = ComputeV(newLength1, alpha)
+            Dim lambda_1 As Double = Computelambda1(e, f, g, u, v)
+            Dim lambda_2 As Double = Computelambda2(e, f, g, u, v)
+
+            'regulateAngle(alpha(i))
+            s(i) = -0.5 * Lab(i) * Lab(i) * (lambda_1 * Math.Cos(alpha(i)) + lambda_2 * Math.Sin(alpha(i)))
+            'end of normalization in edge tweakling
+            MorphL(i) = newLength1(i) + s(i)
             If i = 0 Or i = numofVertex - 1 Then
-                MorphX(i) = CopyFromX(i) * (1 - t) + CopyFromX(i) * t
-                MorphY(i) = CopyFromY(i) * (1 - t) - CopyFromY(i) * t
+                MorphX(i) = CopyFromX(i) * (1 - t) + CopyToX(i) * t
+                MorphY(i) = CopyFromY(i) * (1 - t) + CopyToY(i) * t
             Else
-                'use edge tweakling to close the shape
-                Dim e As Double = ComputeE(MorphAngle)
-                Dim f As Double = ComputeF(MorphAngle)
-                Dim g As Double = ComputeG(MorphAngle)
-                Dim u As Double = ComputeU(newLength1, MorphAngle)
-                Dim v As Double = ComputeV(newLength1, MorphAngle)
-                Dim lambda_1 As Double = Computelambda1(e, f, g, u, v)
-                Dim lambda_2 As Double = Computelambda2(e, f, g, u, v)
-                'Console.WriteLine(lambda_1)
-                'Console.WriteLine(lambda_2)
-
-                regulateAngle(MorphAngle(i))
-                s(i) = -0.5 * Lab(i) * Lab(i) * (lambda_1 * Math.Cos(MorphAngle(i)) + lambda_2 * Math.Sin(MorphAngle(i)))
-                ' Console.WriteLine(s(i))
-                'end of normalization in edge tweakling
-                MorphL(i) = newLength1(i) + s(i)
-
-                MorphX(i) = MorphX(i - 1) + MorphL(i) * Math.Cos(MorphAngle(i))
-                MorphY(i) = MorphY(i - 1) + MorphL(i) * Math.Sin(MorphAngle(i))
+                MorphX(i) = MorphX(i - 1) + MorphL(i - 1) * Math.Cos(alpha(i - 1))
+                MorphY(i) = MorphY(i - 1) + MorphL(i - 1) * Math.Sin(alpha(i - 1))
             End If
         Next
 
-        ' Write to Morph Corrdinate
+        ' Write to Morph Coordinate
         For i = 0 To NumVertexTo - 1
             MorphPoints.Add(New Point(MorphX(i), MorphY(i)))
         Next
+
     End Sub
     Private Function ComputeLtol() As Double
         Dim i As Integer = 0
@@ -710,17 +697,17 @@ Public Class Form1
         Dim max_edge_difference As Double '= 0.0F
 
         For i = 0 To numofVertex - 1
-            edge_difference = Math.Abs(fromLength(i) - toLength(i))
+            edge_difference = Math.Abs(LA(i) - LB(i))
             If (edge_difference > max_edge_difference) Then
                 max_edge_difference = edge_difference
             End If
         Next
-        Return 0.00001 * max_edge_difference
+        Return 0.0001 * max_edge_difference
     End Function
 
     Private Function ComputeE(ByRef alpha) As Double
         Dim e As Double = 0.0F
-        For i = 0 To TotalStep - 1
+        For i = 0 To numofVertex - 1
             e += Lab(i) * Lab(i) * Math.Cos(alpha(i)) * Math.Cos(alpha(i))
             'e = lab^2 + cos(alpha)^2
         Next
@@ -728,7 +715,7 @@ Public Class Form1
     End Function
 
     Private Function ComputeF(ByRef alpha) As Double
-        Dim f As Double = 0.0F
+        Dim f As Double = 0
         For i = 0 To numofVertex - 1
             f += Lab(i) * Lab(i) * Math.Sin(alpha(i)) * Math.Cos(alpha(i))
         Next
@@ -736,7 +723,7 @@ Public Class Form1
     End Function
 
     Private Function ComputeG(ByRef alpha) As Double
-        Dim g As Double = 0.0F
+        Dim g As Double = 0
         For i = 0 To numofVertex - 1
             g += Lab(i) * Lab(i) * Math.Sin(alpha(i)) * Math.Sin(alpha(i))
         Next
@@ -744,7 +731,7 @@ Public Class Form1
     End Function
 
     Private Function ComputeU(ByRef length, ByRef alpha) As Double
-        Dim u As Double = 0.0F
+        Dim u As Double = 0
         For i = 0 To numofVertex - 1
             u += length(i) * Math.Cos(alpha(i))
         Next
@@ -835,41 +822,22 @@ Public Class Form1
         F.Show()
     End Sub
 
-
     Public Sub CreateBitmapAtRuntime()
-        pictureBox1.Size = New Size(1210, 710)
-        'morph panel size is 1200, 780
+        pictureBox1.Size = New Size(210, 110)
         Me.Controls.Add(pictureBox1)
-
-        Dim flag As New Bitmap(1200, 700)
-        Dim flagGraphics As Graphics = Graphics.FromImage(flag)
-        Dim red As Integer = 0
-        Dim white As Integer = 11
-        flagGraphics.FillRectangle(Brushes.White, 0, 0, 1200, 700)
-        While white <= 100
-            flagGraphics.FillRectangle(Brushes.Red, 0, red, 200, 10)
-            flagGraphics.FillRectangle(Brushes.White, 0, white, 200, 10)
-            red += 20
-            white += 20
-        End While
-        flagGraphics.DrawLines(Pens.Blue, FromPoints.ToArray())
-        'flagGraphics.DrawLine(Pens.Red, New Point(MorphX(0), MorphY(0)), New Point(MorphX(1), MorphY(1)))
-
-        pictureBox1.Image = flag
+        Dim newImage As New Bitmap(200, 100)
+        Dim imageGraphics As Graphics = Graphics.FromImage(newImage)
+        imageGraphics.FillRectangle(Brushes.White, 0, 0, 200, 100)
+        imageGraphics.DrawLines(Pens.Black, FromPoints.ToArray())
+        pictureBox1.Image = newImage
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         CreateBitmapAtRuntime()
-        'Dim SaveFileDialog1 As New SaveFileDialog
-        'SaveFileDialog1.FileName = ""
-        'SaveFileDialog1.Filter = "Text Files (*.txt)|*.txt"
-        'SaveFileDialog1.Title = "Please Select Initial Shape File"
-        'SaveFileDialog1.ShowDialog()
-        'Private Sub SaveFileDialog1_FileOk(ByVal sender As System.Object, ByVal e As System.ComponentModel.CancelEventArgs) Handles SaveFileDialog1.FileOk
-        'Dim FileToSaveAs As String = System.IO.Path.Combine(My.Computer.FileSystem.SpecialDirectories.Temp, SaveFileDialog1.FileName)
-        'pictureBox1.Image.Save("C:\Users\Xiaohan Wang\Desktop\Data\newImage", System.Drawing.Imaging.ImageFormat.Jpeg)
         pictureBox1.Image.Save("C:\Users\Xiaohan Wang\Desktop\2D Morph.gif")
     End Sub
 
+    Private Sub GroupBox2_Enter(sender As Object, e As EventArgs) Handles GroupBox2.Enter
 
+    End Sub
 End Class
